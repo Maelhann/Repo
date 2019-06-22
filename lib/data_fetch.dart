5af5ff1c2ac481f81495e4ed5c1320fb854f1bbb
mr3317 <mr3317@ic.ac.uk>
2019-06-22 22:34:34 +0100

    added tw. api credentials

--- lib/data_fetch.dart
+++ lib/data_fetch.dart
@@ -8,7 +8,8 @@ import 'dart:math';
 class DataSearch {
 
   Future<String> getTweeterData(List<String> keywords) async{
-    //TODO ma geule
+    String apiKey = "IKIIecQL9sr0VUwhMVHE9Db9h ";
+    String accessToken = '1142537491192406016-EWGBLYGD6sTf57fNBt984Y1anood0M';
   }
 
   Future<String> getNewsData(List<String> keywords) async{
