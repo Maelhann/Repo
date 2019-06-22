aff6daee5cab4b04c319532f8f3b972ace997a04
mr3317 <mr3317@ic.ac.uk>
2019-06-22 22:43:36 +0100

    added http and async support

--- lib/data_fetch.dart
+++ lib/data_fetch.dart
@@ -4,12 +4,21 @@ import 'package:flutter_tags/input_tags.dart';
 import 'package:flutter_tags/selectable_tags.dart';
 import 'package:flutter/services.dart';
 import 'dart:math';
+import 'dart:async';
+import 'dart:io';
+import 'dart:core';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 class DataSearch {
 
   Future<String> getTweeterData(List<String> keywords) async{
     String apiKey = "IKIIecQL9sr0VUwhMVHE9Db9h ";
     String accessToken = '1142537491192406016-EWGBLYGD6sTf57fNBt984Y1anood0M';
+    String env = "Prod";
+
+
   }
 
   Future<String> getNewsData(List<String> keywords) async{
