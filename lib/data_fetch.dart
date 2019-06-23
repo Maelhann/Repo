25df93f6b0f7f81cd0134d1b5e2c37f62969fc87
mr3317 <mr3317@ic.ac.uk>
2019-06-23 17:57:45 +0100

    added news keys, integrated twitterkit flutter api

--- lib/data_fetch.dart
+++ lib/data_fetch.dart
@@ -9,48 +9,49 @@ import 'dart:io';
 import 'dart:core';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_twitter_login/flutter_twitter_login.dart';
 
 
 class DataSearch {
 
   static Future<String> getTweeterData(List<String> keywords) async{
-    String apiKey = "IKIIecQL9sr0VUwhMVHE9Db9h ";
+    String apiKey = "IKIIecQL9sr0VUwhMVHE9Db9h";
     String secApiKey = "l2EwmYc79GRXInONYN3papcykFLLMHwiQCLNDekeOa22V6QksJ";
 
     String accessToken = '1142537491192406016-EWGBLYGD6sTf57fNBt984Y1anood0M';
     String env = "Prod";
 
-    final tokenAccess = {
-      'user' :  '$apiKey : $secApiKey'
-    };
+
 
 //    curl -u 'API key:API secret key' \
 //    --data 'grant_type=client_credentials' \
 //    'https://api.twitter.com/oauth2/token'
-
-    final getBearerToken = await http.post( 'https://api.twitter.com/oauth2/token',
-      headers: tokenAccess);
-
-    print( getBearerToken.body );
-
-
-
-
-    final url = "https://api.twitter.com/1.1/tweets/search/30day/$env.json";
-    final auth = {
-      'authorization' : 'Bearer $accessToken',
-      'content-type' : 'application/json'
-    };
+//
+//    final tokenAccess = {
+//      'Authorization':  '$apiKey:$secApiKey'
+//    };
+//    final getBearerToken = await http.post( 'https://api.twitter.com/oauth2/token',
+//      headers: tokenAccess,body:'grant_type=client_credentials');
+//
+//    print( getBearerToken.body );
 
 
-    final request = await http.post(url,headers : auth);
-    print(request.body);
-    return request.body;
+//    final url = "https://api.twitter.com/1.1/tweets/search/30day/$env.json";
+//    final auth = {
+//      'authorization' : 'Bearer $accessToken',
+//      'content-type' : 'application/json'
+//    };
+//
+//
+//    final request = await http.post(url,headers : auth);
+//    print(request.body);
+//    return request.body;
 
   }
 
   Future<String> getNewsData(List<String> keywords) async{
-    //TODO ma bonne race
+    // api key c83a25050dc54eada0e9c6fff9f2ff44
+    // use curl as always.
   }
 
 }
